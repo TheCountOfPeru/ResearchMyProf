25fed2108fbb4a613636e838f8f7ef762b37a442
Jonathan Yee <webber989@gmail.com>
2018-06-07 17:10:36 -0600

    added some starting pages

---
+++ splashpage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+Jonathan Yee
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ResearchMyProf - title</title>
+    </head>
+    <body>
+        <h1 align="center"
+            style="font-family:consolas">ResearchMyProf</h1>
+        <hr>
+        <table align="center">
+            <tr>
+                <td><a href="splashpage.php">Search</a></td>
+                <td><a href="loginpage.php">Login</a></td>
+                <td><a href="aboutpage.php">About</a></td>
+            </tr>
+        </table>
+        
+            
+        <?php
+        
+        ?>
+    </body>
+</html>
