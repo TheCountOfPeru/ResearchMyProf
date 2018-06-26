b47fe519e61dc70be1d9a38d89a2cf6bb7d81094
Jonathan Yee <webber989@gmail.com>
2018-06-25 23:01:44 -0600

    fixed some errors

--- administration.php
+++ administration.php
@@ -54,7 +54,7 @@ include('session.php');
                 <a href="logout.php" class="w3-button w3-blue w3-round w3-ripple w3-xlarge" style="width: 20%">Logout</a>
             </div>
     </div>
-        <form class="w3-cell-row w3-border w3-centered w3-light-grey w3-animate-input">
+        <form class="w3-cell-row w3-border w3-centered w3-light-grey w3-animate-input" action="" align="center" method="post">
 
         <?php
         $sql = "SELECT user.user_id, user.username, user.is_admin, user.is_mod, user.start_date
--- searchpage.php
+++ searchpage.php
@@ -24,23 +24,30 @@ and open the template in the editor.
             }else{
                 $query = mysqli_real_escape_string($db,filter_input(INPUT_GET, 'query'));
                 $query = trim($query);
-                $sql = "SELECT profile.profile_id, profile.name, interested_in.tname, institution.i_name, location.country, location.city
-                        FROM profile JOIN location ON profile.I_postal=location.postal_code
-						 JOIN institution ON profile.I_postal=institution.postal_code
-						 JOIN interested_in ON profile.profile_id=interested_in.profile_id
-                        WHERE";
-                if($select == 'country'){
-                    $sql = $sql." location.country LIKE '%".$query."%'";
-                }elseif($select == 'institution'){
-                    $sql = $sql." institution.i_name LIKE '%".$query."%'";
-                }elseif($select == 'topic'){
-                    $sql = $sql." interested_in.tname LIKE '%".$query."%'";
+                
+                if($select == 'country'){//Search by country name
+                    $sql = "SELECT profile.profile_id, profile.name, institution.i_name, location.country, location.city
+                            FROM profile JOIN location ON profile.I_postal=location.postal_code
+                                         JOIN institution ON profile.I_postal=institution.postal_code
+                                         WHERE location.country LIKE '%";
+                    $sql = $sql.$query."%'";
+                }elseif($select == 'institution'){//Search by institution name
+                    $sql = "SELECT profile.profile_id, profile.name, institution.i_name, location.country, location.city
+                            FROM profile JOIN location ON profile.I_postal=location.postal_code
+                                         JOIN institution ON profile.I_postal=institution.postal_code
+                                         WHERE institution.i_name LIKE '%";
+                    $sql = $sql.$query."%'";
+                }elseif($select == 'topic'){//Search ]by topic name
+                    //Find profiles based on interested topics first - need exact match
+                    $sql = "SELECT interested_in.profile_id
+                            FROM interested_in
+                            WHERE tname='";
+                    $sql = $sql.strtolower($query)."'";
                 }
                 $result = mysqli_query($db,$sql);
             }
            
             
-
           }
           
           function test_input($data) {
@@ -91,29 +98,79 @@ and open the template in the editor.
 
 
 
-        <?php
-            if($query != "" || $select != ""){
+       <?php
+            if($query != "" || $select != ""){//check if query is empty
                 if ($result->num_rows > 0) {
                     // output data of each row
-                    echo "<table class=\"w3-table w4-container\"><tr>
+                    echo "<table align='center' //Print table headers
+                            style='width:50%'
+                            border='1'><tr>
                             <th>Name</th>
-                            <th>Topic</th>
                             <th>Institution</th>
                             <th>Country</th>
                             <th>City</th>
+                            <th>Topic(s)</th>
                             </tr>";
-                    while($row = $result->fetch_assoc()) {
+                    if($select == 'country' || $select == 'institution'){
+                        while($row = $result->fetch_assoc()) {
                         echo "<tr>";
                         echo "<td><a href='profile.php?id=".$row["profile_id"]."'>".$row["name"]."</a></td>";
-                        echo "<td>".$row["tname"]."</td>";//Should change this to show all topics related to profile, dont make another row of the same profile
+                        //echo "<td>".$row["tname"]."</td>";//Should change this to show all topics related to profile, dont make another row of the same profile
                         echo "<td>".$row["i_name"]."</td>";
                         echo "<td>".$row["country"]."</td>";
                         echo "<td>".$row["city"]."</td>";
+                        
+                        //Query for the profile_id interested in topics
+                        echo "<td>";
+                        echo "<table>";
+                        $tsql = "SELECT tname FROM interested_in WHERE profile_id=".$row["profile_id"];
+                        $tresult = mysqli_query($db,$tsql);
+                        //print out the topics into the last column
+                        while($trow = $tresult->fetch_assoc()){
+                            echo $trow["tname"]."<br>";
+                            }
+                        echo "</table>";
+                        echo "</td>";
                         echo "</tr>";
+                        }
+                    }elseif($select == 'topic'){
+                        
+                        while($row = $result->fetch_assoc()){//loop for print each row
+                            $p_id = $row["profile_id"];
+                            //from the row's profile_id find other info to print, need another query
+                            $tsql = "SELECT profile.profile_id, profile.name, institution.i_name, location.country, location.city
+                                     FROM profile JOIN location ON profile.I_postal=location.postal_code
+                                             JOIN institution ON profile.I_postal=institution.postal_code
+                                     WHERE profile.profile_id=".$p_id;
+                            $tresult = mysqli_query($db,$tsql);//returns info about a profile_id. should only get 1 profile
+                            $first_row = $tresult->fetch_assoc();
+                        echo "<tr>";
+                        echo "<td><a href='profile.php?id=".$first_row["profile_id"]."'>".$first_row["name"]."</a></td>";
+                        //echo "<td>".$row["tname"]."</td>";//Should change this to show all topics related to profile, dont make another row of the same profile
+                        echo "<td>".$first_row["i_name"]."</td>";
+                        echo "<td>".$first_row["country"]."</td>";
+                        echo "<td>".$first_row["city"]."</td>";
+                        
+                        //Query for the profile_id interested in topics
+                        echo "<td>";
+                        echo "<table>";
+                        $tsql = "SELECT tname FROM interested_in WHERE profile_id=".$p_id;
+                        $tresult = mysqli_query($db,$tsql);
+                        //print out the topics into the last column
+                        while($trow = $tresult->fetch_assoc()){
+                            echo $trow["tname"]."<br>";
+                            }
+                        echo "</table>";
+                        echo "</td>";
+                        echo "</tr>";
+                        }
+                        
                     }
-                    echo "</table><br><br>";
+                    
+                    
+                    echo "</table>";
                 } else {
-                    echo "<p align='center'>0 results</p><br><br>";
+                    echo "<p align='center'>0 results</p>";
                 }
             }
             ?>
